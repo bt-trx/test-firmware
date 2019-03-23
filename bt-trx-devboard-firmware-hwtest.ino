ec91ec96df0c2f83979ed93daabaf64541eae527
chris <mail@chris007.de>
2019-03-23 17:22:36 +0100

    changed sketch for teensy3.2

--- bt-trx-devboard-firmware-hwtest.ino
+++ bt-trx-devboard-firmware-hwtest.ino
@@ -1,54 +1,30 @@
 /*
   Software serial multple serial test
+WT32i:
+TX: Pin7
+RX: Pin8
 
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
+VREGENA: Pin 12
  */
-#include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(3, 2); // RX, TX
+#define PIN_WT32_ONOFF 20
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial3.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
-
-  Serial.println("Goodnight moon!");
-
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
-  mySerial.println("Hello, world?");
+  Serial.println("Hello!");
+  pinMode(PIN_WT32_ONOFF, OUTPUT);
+  digitalWrite(PIN_WT32_ONOFF, HIGH);
 }
 
 void loop() { // run over and over
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
+  if (Serial3.available()) {
+    Serial.write(Serial3.read());
   }
   if (Serial.available()) {
-    mySerial.write(Serial.read());
+    Serial3.write(Serial.read());
   }
 }
