d2245e86a916b407a07ba33711b1880028538301
chris007de <mail@chris007.de>
2019-04-12 21:22:19 +0200

    Added test for button and LEDs

--- bt-trx-devboard-firmware-hwtest.ino
+++ bt-trx-devboard-firmware-hwtest.ino
@@ -1,12 +1,10 @@
 /*
-  Software serial multple serial test
-WT32i:
-TX: Pin7
-RX: Pin8
-
-VREGENA: Pin 12
+  Hardware test for bt-trx devboard v2
  */
-#define PIN_WT32_ONOFF 20
+#define PIN_WT32_ONOFF 13
+#define PIN_LED0 3
+#define PIN_LED1 4
+#define PIN_BTN0 23
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -15,8 +13,12 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.println("Hello!");
+  Serial.println("bt-trx Hardware Test");
   pinMode(PIN_WT32_ONOFF, OUTPUT);
+  pinMode(PIN_LED0, OUTPUT);
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_BTN0, INPUT);
+  
   digitalWrite(PIN_WT32_ONOFF, HIGH);
 }
 
@@ -26,5 +28,13 @@ void loop() { // run over and over
   }
   if (Serial.available()) {
     Serial3.write(Serial.read());
+  }
+
+  if (digitalRead(PIN_BTN0)) {
+    digitalWrite(PIN_LED0, LOW);
+    digitalWrite(PIN_LED1, HIGH);
+  } else {
+    digitalWrite(PIN_LED0, HIGH);
+    digitalWrite(PIN_LED1, LOW);
   }
 }
