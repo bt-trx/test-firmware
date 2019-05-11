9dcc7da4277d3406b839cbc942e473e7856f6880
chris <mail@chris007.de>
2019-05-11 22:55:35 +0200

    Add comments and license

--- bt-trx-devboard-firmware-hwtest.ino
+++ bt-trx-devboard-firmware-hwtest.ino
@@ -1,6 +1,23 @@
 /*
-  Hardware test for bt-trx devboard v2
- */
+This file is part of bt-trx
+
+bt-trx is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+bt-trx is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2019 Christian Obersteiner (DL1COM), Andreas Müller (DC1MIL)
+Contact: bt-trx.com, mail@bt-trx.com
+*/
+
 #define PIN_WT32_ONOFF 13
 #define PIN_LED0 3
 #define PIN_LED1 4
@@ -18,11 +35,13 @@ void setup() {
   pinMode(PIN_LED0, OUTPUT);
   pinMode(PIN_LED1, OUTPUT);
   pinMode(PIN_BTN0, INPUT);
-  
+
+  // Get WT32i out of reset
   digitalWrite(PIN_WT32_ONOFF, HIGH);
 }
 
 void loop() { // run over and over
+  // Pipe serial data from/to WT32i to USB Serial
   if (Serial3.available()) {
     Serial.write(Serial3.read());
   }
@@ -30,6 +49,8 @@ void loop() { // run over and over
     Serial3.write(Serial.read());
   }
 
+  // If the button is pressed, light up green LED
+  // If the button is not pressen, light up blue LED
   if (digitalRead(PIN_BTN0)) {
     digitalWrite(PIN_LED0, LOW);
     digitalWrite(PIN_LED1, HIGH);
